55a8a4f538c60cafb7f5f55991dd964322ec527a
Kendra Kleber <league@Mac-mini-2.local>
2019-03-23 11:25:21 -0700

    3-23-19
    started to lay off people

---
+++ src/Layoffs/Layoffs.java
@@ -0,0 +1,13 @@
+package Layoffs;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Layoffs {
+	private JFrame frame = new JFrame();
+	private JPanel panel = new JPanel();
+	
+	public static void main(String[] args) {
+		
+	}
+}
